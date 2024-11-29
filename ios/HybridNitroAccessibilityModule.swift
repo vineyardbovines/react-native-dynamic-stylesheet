1e65969c718e187adfb3da26862255935fb0a83d
vineyardbovines <spencerfpope@gmail.com>
2024-11-28 19:56:22 -0500

    fix: differentiate without color

--- ios/HybridNitroAccessibilityModule.swift
+++ ios/HybridNitroAccessibilityModule.swift
@@ -118,7 +118,7 @@ class HybridNitroAccessibilityModule : HybridNitroAccessibilityModuleSpec {
     public func getIsDifferentiateWithoutColorEnabled() throws -> Bool? {
         func getIsDifferentiateWithoutColorEnabledFn() -> Bool? {
             if #available(iOS 13.0, *) {
-                return UIAccessibility.isDifferentiateWithoutColorEnabled
+                return UIAccessibility.shouldDifferentiateWithoutColorEnabled
             } else {
                 return nil
             }
